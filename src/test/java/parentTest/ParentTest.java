32c2fad85e31a0c3b0b5ef1f205245a1ed37960b
Aleksandra Chornoivanova <achornoivanova@gmail.com>
2018-07-08 16:51:56 +0300

    20-06-Part1 test with page object

---
+++ src/test/java/parentTest/ParentTest.java
@@ -0,0 +1,25 @@
+package parentTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+public class ParentTest {
+    WebDriver webDriver;
+    @Before
+    public void setUp (){
+        File file = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        webDriver = new ChromeDriver();
+    }
+
+    @After
+
+    public void tearDown (){
+        webDriver.quit();
+    }
+}
---
+++ src/test/java/signInTests/SignInTestWithPageObject.java
@@ -0,0 +1,11 @@
+package signInTests;
+
+import org.junit.Test;
+import parentTest.ParentTest;
+
+public class SignInTestWithPageObject extends ParentTest {
+    @Test
+    public void validSignIn() {
+
+    }
+}
