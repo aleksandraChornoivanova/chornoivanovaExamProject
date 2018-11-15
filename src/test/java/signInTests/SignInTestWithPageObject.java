5b20e92eb62c8698f92bc55ad8b65234f3741ded
Aleksandra Chornoivanova <achornoivanova@gmail.com>
2018-11-15 23:10:52 +0200

    15-11-2018 valid test with page object using @FindBy

--- src/test/java/signInTests/SignInTestWithPageObject.java
+++ src/test/java/signInTests/SignInTestWithPageObject.java
@@ -1,13 +1,16 @@
 package signInTests;
 
-import org.junit.Test;
-import parentTest.ParentTest;
+        import org.junit.Test;
+        import parentTest.ParentTest;
 
 public class SignInTestWithPageObject extends ParentTest {
     @Test
     public void validSignIn() {
         homePage.openPage();
+        homePage.clickSignInButton();
         homePage.enterLogIn("ac@nustay.com");
+        homePage.enterPassword("111111");
+        homePage.clickContinueButton();
 
     }
 }
